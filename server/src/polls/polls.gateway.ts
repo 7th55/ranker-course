5dba7d0e2ea173190e6484475b19a7dea10ef3b0
7th55 <7th55hh@gmail.com>
2022-12-10 21:22:26 +0400

    Added Socket Authorization

--- server/src/polls/polls.gateway.ts
+++ server/src/polls/polls.gateway.ts
@@ -6,8 +6,9 @@ import {
   OnGatewayDisconnect,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Namespace, Socket } from 'socket.io';
+import { Namespace } from 'socket.io';
 import { PollsService } from './polls.service';
+import { SocketWithAuth } from './types';
 
 @WebSocketGateway({
   namespace: 'polls',
@@ -24,18 +25,24 @@ export class PollsGateway
 
   @WebSocketServer() io: Namespace;
 
-  handleConnection(client: Socket) {
+  handleConnection(client: SocketWithAuth) {
     const sockets = this.io.sockets;
 
+    this.logger.debug(
+      `Socket connected with userID: ${client.userID}, pollID: ${client.pollID}, and name "${client.name}"`,
+    );
     this.logger.log(`WS Client with id: ${client.id} connected!`);
     this.logger.debug(`Number of connected sockets: ${sockets.size}`);
 
     this.io.emit('hello', `from ${client.id}`);
   }
 
-  handleDisconnect(client: Socket) {
+  handleDisconnect(client: SocketWithAuth) {
     const sockets = this.io.sockets;
 
+    this.logger.debug(
+      `Socket disconnected with userID: ${client.userID}, pollID: ${client.pollID}, and name "${client.name}"`,
+    );
     this.logger.log(`Disconnected socket id: ${client.id}`);
     this.logger.debug(`Number of connected sockets: ${sockets.size}`);
   }
